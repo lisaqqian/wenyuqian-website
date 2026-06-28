a7c9ac629a8c347713000a4a7c81429f7605ebb3
lisaqqian <wenyuqian22@gmail.com>
2026-06-28 16:09:25 -0700

    Update dropped.js

--- dropped.js
+++ dropped.js
@@ -3,4 +3,10 @@
 
 const dropped = [
   // { title: "Example Book", author: "Author Name", url: "https://app.thestorygraph.com/books/search?q=example", fave: false },
+  { title: "Onyx Storm", author: "Rebecca Yarros", url: "https://app.thestorygraph.com/books/29ee0457-ee9a-44c9-ab8d-6dfea65ead1e", fave: false },   
+  { title: "Smoke and Mirrors: Short Fictions and Illusions", author: "Neil Gaiman", url: "https://app.thestorygraph.com/books/b9ad367f-ad1f-4d46-80ae-42ae8e586fa0", fave: false },
+  { title: "The Unwanteds", author: "Lisa McMann", url: "https://app.thestorygraph.com/books/2f9c9f73-e70d-4e19-87fd-6d67d91536c4", fave: false },
+  { title: "The Rage of Dragons", author: "Evan Winter", url: "https://app.thestorygraph.com/books/9d61f6b8-08cf-4394-9e86-0b918788fd24", fave: false },
+
+  
 ];
