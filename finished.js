b229625cd1d32372eb4e09c923ae90f495d35aec
lisaqqian <wenyuqian22@gmail.com>
2026-06-28 15:59:29 -0700

    Update finished.js

--- finished.js
+++ finished.js
@@ -4,6 +4,6 @@
 
 const finished = [
   // { title: "Red Rising", author: "Pierce Brown", url: "https://app.thestorygraph.com/books/search?q=red+rising", fave: true },
-  { title: "Silver Elite", author: "Victoria Aveyard", url: "https://app.thestorygraph.com/books/search?q=silver+elite", fave: false },
-  { title: "Golden Son",   author: "Pierce Brown",     url: "https://app.thestorygraph.com/books/search?q=golden+son",   fave: true },
+  { title: "Silver Elite", author: "Victoria Aveyard", url: "https://app.thestorygraph.com/books/f3f995a3-eac7-49d9-889c-4c6a318055db", fave: false },
+  { title: "Golden Son",   author: "Pierce Brown",     url: "https://app.thestorygraph.com/books/1d029926-2295-49a4-88f7-e1767885baf2",   fave: true },
 ];
