e0527c2d7ac13cb6d191be38448b64122e4be8f0
lisaqqian <wenyuqian22@gmail.com>
2026-06-28 15:58:46 -0700

    Add 'The Night Circus' to reading list

--- reading.js
+++ reading.js
@@ -3,6 +3,5 @@
 // fave: true adds a star next to the title.
 
 const reading = [
-  { title: "Silver Elite", author: "Victoria Aveyard", url: "https://app.thestorygraph.com/books/search?q=silver+elite", fave: false },
-  { title: "Golden Son",   author: "Pierce Brown",     url: "https://app.thestorygraph.com/books/search?q=golden+son",   fave: false },
+  { title: "The Night circus", author: "Erin Morgenstern", url: "https://app.thestorygraph.com/books/b5e3d973-9cbb-4fa5-aa80-ab0d305c7ddc", fave: false },
 ];
