5cc6abc80a9230a6e2bb36fe3ac96dd6a0c761a0
lisaqqian <wenyuqian22@gmail.com>
2026-06-28 16:05:54 -0700

    Update finished.js

--- finished.js
+++ finished.js
@@ -4,6 +4,16 @@
 
 const finished = [
   // { title: "Red Rising", author: "Pierce Brown", url: "https://app.thestorygraph.com/books/search?q=red+rising", fave: true },
-  { title: "Silver Elite", author: "Victoria Aveyard", url: "https://app.thestorygraph.com/books/f3f995a3-eac7-49d9-889c-4c6a318055db", fave: false },
   { title: "Golden Son",   author: "Pierce Brown",     url: "https://app.thestorygraph.com/books/1d029926-2295-49a4-88f7-e1767885baf2",   fave: true },
+  { title: "Silver Elite", author: "Victoria Aveyard", url: "https://app.thestorygraph.com/books/f3f995a3-eac7-49d9-889c-4c6a318055db", fave: false },
+  { title: "Red Rising", author: "Pierce Brown", url: "https://app.thestorygraph.com/books/240f2048-8438-49b7-90a3-a5dc1018270f", fave: true },
+  { title: "The Song of Achilles", author: "Madeline Miller", url: "https://app.thestorygraph.com/books/2244c7ad-dc04-4daf-8918-a0350ee22c2f", fave: false },
+  { title: "Iron Flame", author: "Rebecca Yarros", url: "https://app.thestorygraph.com/books/c86c4a48-89aa-4805-a513-2fef489d8420", fave: false },
+  { title: "The Ballad of Songbirds and Snakes", author: "Suzanne Collins", url: "https://app.thestorygraph.com/books/da391079-005c-41bf-8c1a-0fb155cd7677", fave: false },
+  { title: "Ruthless Vows", author: "Rebecca Ross", url: "https://app.thestorygraph.com/books/68503084-c5a2-4eef-90e8-45dd9c8418ee", fave: false },
+  { title: "Fourth Wing", author: "Rebecca Yarros", url: "https://app.thestorygraph.com/books/b8a70ef8-123c-44d8-bd6d-2f3ce498406e", fave: true },
+  { title: "Mockingjay", author: "Suzanne Collins", url: "https://app.thestorygraph.com/books/037b8b82-cc25-47e2-b47a-d9b3f4a0625c", fave: false },
+  { title: "Catching Fire", author: "Suzanne Collins", url: "https://app.thestorygraph.com/books/84c343bd-8861-49a2-9301-62f3bb6da9ae", fave: true },
+  { title: "The Hunger Games", author: "Suzanne Collins", url: "https://app.thestorygraph.com/books/03574368-abfc-4c75-823f-451a4301528f", fave: false },
+  { title: "Divine Rivals", author: "Rebecca Ross", url: "https://app.thestorygraph.com/books/8145fb3d-8156-43f9-be7d-f8c656f81a6d", fave: true },
 ];
