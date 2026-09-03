1e64afde59b6d25540be5eb6c5674c5272259c26
lisaqqian <wenyuqian22@gmail.com>
2026-09-03 00:34:28 -0700

    Fix syntax error in reading.js

--- reading.js
+++ reading.js
@@ -3,5 +3,5 @@
 // fave: true adds a star next to the title.
 
 const reading = [
-  { title: "The Night circus", author: "Erin Morgenstern", url: "https://app.thestorygraph.com/books/b5e3d973-9cbb-4fa5-aa80-ab0d305c7ddc", fave: false },
-];
+//  { title: "The Night circus", author: "Erin Morgenstern", url: "https://app.thestorygraph.com/books/b5e3d973-9cbb-4fa5-aa80-ab0d305c7ddc", fave: false },
+  ];
